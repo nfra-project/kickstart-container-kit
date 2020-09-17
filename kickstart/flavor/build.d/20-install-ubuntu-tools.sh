3e54653d52ac7c681b7140b1ca8f3a0012f8b0ec
Matthes <matthes@leuffen.de>
2020-09-17 18:52:45 +0200

    fix locale gen

--- kickstart/flavor/build.d/20-install-ubuntu-tools.sh
+++ kickstart/flavor/build.d/20-install-ubuntu-tools.sh
@@ -16,8 +16,8 @@ else
 
     # Install raw packages (reduce size)
     apt-get install -y --no-install-recommends git openssh-client vim pwgen telnet
+    locale-gen en_US.UTF-8
 fi;
 # Packages that have less recommends
 
 
-locale-gen en_US.UTF-8

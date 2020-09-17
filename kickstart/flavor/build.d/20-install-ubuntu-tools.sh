e15b0a8d0c36376f79e9a86edf7fe13847a5e0a3
Matthes <matthes@leuffen.de>
2020-09-17 18:23:19 +0200

    fix tools

--- kickstart/flavor/build.d/20-install-ubuntu-tools.sh
+++ kickstart/flavor/build.d/20-install-ubuntu-tools.sh
@@ -2,10 +2,22 @@
 
 set -e
 
-# Packages that have less recommends
-apt-get install -y --no-install-recommends zip whois bash-completion locales
+###
+# Developer tools
+#
+# DOCKER_TAG is only availabe during build phase (defined in flavors dockerfile)
+##
 
-# Install raw packages (reduce size)
-apt-get install -y --no-install-recommends git openssh-client vim pwgen
+if [[ ${DOCKER_TAG} =~ \-min$ ]]
+then
+    echo "This is a '-min' build; Skipping developer features"
+else
+    apt-get install -y --no-install-recommends zip whois bash-completion locales
+
+    # Install raw packages (reduce size)
+    apt-get install -y --no-install-recommends git openssh-client vim pwgen telnet
+fi;
+# Packages that have less recommends
+
 
 locale-gen en_US.UTF-8

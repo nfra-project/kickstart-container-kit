64e441aa563ced49fc019da57164e9117c7923cc
Matthes <matthes@leuffen.de>
2021-02-12 17:18:16 +0100

    fix config write

--- kickstart/run/entrypoint.sh
+++ kickstart/run/entrypoint.sh
@@ -193,10 +193,10 @@ else
 
 
 
-    info "Running 'kick write_config_file'"
-    sudo -E -s -u user /kickstart/bin/kick write_config_file
+    info "Running 'kick write_config_file' (root)"
+    /kickstart/bin/kick write_config_file
 
-    info "Running 'kick init'"
+    info "Running 'kick init' (user)"
     sudo -E -s -u user /kickstart/bin/kick init
 
     info "Running '$__DIR__/start.d/'";

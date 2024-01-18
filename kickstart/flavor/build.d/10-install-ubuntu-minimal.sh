e6e799de6ef3e46c125f745eedb9bc5bdfe51165
Matthias Leuffen <matthes@leuffen.de>
2024-01-18 11:03:12 +0100

    added patch command

--- kickstart/flavor/build.d/10-install-ubuntu-minimal.sh
+++ kickstart/flavor/build.d/10-install-ubuntu-minimal.sh
@@ -11,7 +11,7 @@ apt-get upgrade -y
 
 # Bug in Ubuntu18.04. You have to install tzdata standalone first!
 apt-get install -y --no-install-recommends tzdata
-apt-get install -y --no-install-recommends curl sudo ca-certificates unzip git
+apt-get install -y --no-install-recommends curl sudo ca-certificates unzip git patch
 
 
 
